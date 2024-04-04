0e891460c87d0d092eee621ede2931d641272eaa
bmancilla23 <bmancilla119@gmail.com>
2024-04-04 06:01:28 -0500

    Style component para la pagina home

---
+++ src/styled-components/layout.styled.component.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const LayoutContainer = styled.section`
+
+    @media only screen and (max-width: 1400px){
+        width: 100%;
+        padding: 2rem;
+    }    
+
+    @media only screen and (max-width: 1200px){
+        padding: .8rem;
+    }
+
+`
